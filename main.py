9cf19e48238530b36ca8ab90acc9110ccc711ae9
CuiiGen <1029374457@qq.com>
2022-05-24 10:12:58 +0800

    基本完成

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
-from time import localtime, strftime, time
+from os import makedirs
+from time import localtime, strftime
 
 import requests
 from lxml import etree
@@ -13,6 +14,47 @@ logging.basicConfig(
 
 # 相对路径
 path = strftime('%Y_%m%d_%H_%M%S', localtime())
+# 建立文件夹
+makedirs(f'{path}/complain_item')
+# cookie
+cookie = 'ihome_sendmail=1; ihome_loginuser=ZY2003503; ihome_seccode=e477uSo/C8Av2V/JQ/+BDdfw8CFs1GxDG+8LWC/Xdf8g; ihome_auth=8e61vWCQqJj/09aiFDBzPj2oGOkuvMM0ehshR95/rHXPbgBoVUJK37R3fnGhXtDUukWv63AvtMyItLMCFazbQHQFhg; ihome_checkpm=1'
+
+
+def getDetail(doid):
+    '''
+    获取问题及回复详情页面
+    :doid
+    '''
+    # 基本信息
+    url = 'http://i.buaa.edu.cn/space.php'
+    param = {'do': 'complain_item', 'doid': doid}
+    # 请求头
+    header = {
+        'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Cookie': cookie,
+        'Host': 'i.buaa.edu.cn',
+        'Referer': 'http://i.buaa.edu.cn/space.php?do=complain',
+        'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+        'Upgrade-Insecure-Requests': '1'
+    }
+    # 请求
+    r = requests.get(url, params=param, headers=header)
+    r.encoding = 'utf-8'
+    # 解析
+    h = etree.HTML(r.text)
+    # 获取信息
+    li = h.xpath('//*[@id="content"]/div[2]/ol/li')
+    detail = etree.tostring(li[0], encoding='utf-8', method='html').decode('utf-8')
+    # 保存
+    with open(f'{path}/complain_item/{doid}.html', 'w', encoding='utf-8') as f:
+        f.write(detail)
+        f.flush()
 
 
 def main():
@@ -32,8 +74,7 @@ def main():
         'Connection': 'keep-alive',
         'Content-Length': '7',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie':
-        'ihome_sendmail=1; ihome_loginuser=ZY2003503; ihome_seccode=e477uSo/C8Av2V/JQ/+BDdfw8CFs1GxDG+8LWC/Xdf8g; ihome_auth=8e61vWCQqJj/09aiFDBzPj2oGOkuvMM0ehshR95/rHXPbgBoVUJK37R3fnGhXtDUukWv63AvtMyItLMCFazbQHQFhg; ihome_checkpm=1',
+        'Cookie': cookie,
         'Host': 'i.buaa.edu.cn',
         'Origin': 'http://i.buaa.edu.cn',
         'Referer': 'http://i.buaa.edu.cn/space.php?do=complain',
@@ -60,13 +101,14 @@ def main():
             break
         # 修改href
         for i in href:
-            j = i.attrib.get('href')
-            print(j)
-            i.set('href', f'complain_item/{j[32:]}.html')
+            j = i.attrib.get('href')[32:]
+            logging.info(f'详情{j}')
+            getDetail(j)
+            i.set('href', f'complain_item/{j}.html')
         # 获取列表
         complain_list = h.xpath('//div[@class="complain_list_container"]/table[@class="complain_list"]')
         # 获取outerhtml
-        table = etree.tostring(complain_list[0], encoding='utf-8').decode('utf-8')
+        table = etree.tostring(complain_list[0], encoding='utf-8',method='html').decode('utf-8')
         # 保存结果
         with open(f'{path}/a.html', 'a', encoding='utf-8') as f:
             f.write(table)
