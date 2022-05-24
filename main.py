05dcc1735066247250afeaf917be7bfb0fea312a
CuiiGen <1029374457@qq.com>
2022-05-24 09:20:52 +0800

    完成列表所有数据获取

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
+import logging
+from time import localtime, strftime, time
+
 import requests
 from lxml import etree
 
+# 配置logging模块
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - "%(pathname)s", line %(lineno)d - %(funcName)s - %(message)s'
+)
+
+# 相对路径
+path = strftime('%Y_%m%d_%H_%M%S', localtime())
+
 
 def main():
+    '''
+    主函数，获取诉求列表
+    '''
+    # 基本信息
     url = 'http://i.buaa.edu.cn/space.php'
     param = {'do': 'complain', 'view': 'all', 'type': 'all', 'page': 2}
     form = {'atuid': 0}
 
+    # 请求头
     header = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate',
@@ -16,7 +33,7 @@ def main():
         'Content-Length': '7',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Cookie':
-        'ihome_loginuser=ZY2003503; UM_distinctid=18068e837dd141-00f2d0e465691d-6b3e555b-384000-18068e837dec87; Hm_lvt_8edeba7d3ae859d72148a873531e0fa5=1653295087,1653299629,1653305417,1653306778; Hm_lpvt_8edeba7d3ae859d72148a873531e0fa5=1653306778; ihome_seccode=b203KDrJ36UkYUIbuQjQm0r69GpzpuPyReHi86g4gHmL; ihome_auth=1b4ce2kWCim6wZ2GVnhkYAEe8AGLk1W6EQCTnuoerx2Ld7%2B%2FTmFFfg63%2FojHBvprB5hF80Ku%2F6Rv9Fo7%2BzDFRjGb3g; ihome_sendmail=1; ihome_checkpm=1',
+        'ihome_sendmail=1; ihome_loginuser=ZY2003503; ihome_seccode=e477uSo/C8Av2V/JQ/+BDdfw8CFs1GxDG+8LWC/Xdf8g; ihome_auth=8e61vWCQqJj/09aiFDBzPj2oGOkuvMM0ehshR95/rHXPbgBoVUJK37R3fnGhXtDUukWv63AvtMyItLMCFazbQHQFhg; ihome_checkpm=1',
         'Host': 'i.buaa.edu.cn',
         'Origin': 'http://i.buaa.edu.cn',
         'Referer': 'http://i.buaa.edu.cn/space.php?do=complain',
@@ -25,22 +42,37 @@ def main():
         'X-Requested-With': 'XMLHttpRequest'
     }
 
-    r = requests.post(url, params=param, data=form, headers=header)
-    r.encoding = 'utf-8'
-    h = etree.HTML(r.text)
-    href = h.xpath(
-        '//div[@class="complain_list_container"]/table[@class="complain_list"]/tbody/tr/td[@class="info_tip "]/a'
-    )
-    for i in href:
-        j = i.attrib.get('href')
-        print(j)
-        i.set('href', f'complain_item/{j[32:]}.html')
-    complain_list = h.xpath(
-        '//div[@class="complain_list_container"]/table[@class="complain_list"]')
-    table = etree.tostring(complain_list[0], encoding='utf-8').decode('utf-8')
-    with open('a.html', 'w', encoding='utf-8') as f:
-        f.write(table)
-        f.flush()
+    # 页码循环
+    page = 1
+    while True:
+        # 更新QueryParameters
+        param['page'] = page
+        logging.info(f'获取页面{page}的信息')
+        # 获取请求
+        r = requests.post(url, params=param, data=form, headers=header)
+        r.encoding = 'utf-8'
+        # 解析
+        h = etree.HTML(r.text)
+        # 获取href
+        href = h.xpath('//div[@class="complain_list_container"]/table[@class="complain_list"]/tbody/tr/td[1]/a')
+        # 检查是否空页面
+        if len(href) == 0:
+            break
+        # 修改href
+        for i in href:
+            j = i.attrib.get('href')
+            print(j)
+            i.set('href', f'complain_item/{j[32:]}.html')
+        # 获取列表
+        complain_list = h.xpath('//div[@class="complain_list_container"]/table[@class="complain_list"]')
+        # 获取outerhtml
+        table = etree.tostring(complain_list[0], encoding='utf-8').decode('utf-8')
+        # 保存结果
+        with open(f'{path}/a.html', 'a', encoding='utf-8') as f:
+            f.write(table)
+            f.flush()
+        # 下一页
+        page += 1
 
 
 if __name__ == '__main__':
